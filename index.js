7912a1a6b3c0ca993dbf2d8619eb4d91fe28df96
Beringer Dorottya <beringerdorottya@gmail.com>
2024-02-06 11:04:52 +0100

    added tools to convenient working in local

--- index.js
+++ index.js
@@ -36,6 +36,6 @@ app.get(
 );
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT || 3300, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
